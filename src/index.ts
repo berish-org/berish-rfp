8aa3e61520c051319e0b348fc01b4f9de57cc1d2
Berishev Ravil <quadraticteam@gmail.com>
2021-02-26 13:02:02 +0300

    Fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-import * as constants from './constants';
-import * as helpers from './helpers';
+export * as constants from './constants';
+export * as helpers from './helpers';
 export * as errors from './errors';
 
 export * from './peer';
 export * from './modules';
 export * from './serber';
-
-export { constants, helpers };
+export * from './transport';
