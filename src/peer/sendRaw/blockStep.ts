88d90bf922f13e4ef7d76a928e52b7d48539f4bd
Berishev Ravil <quadraticteam@gmail.com>
2021-03-06 20:21:43 +0300

    Fix blocking

--- src/peer/sendRaw/blockStep.ts
+++ src/peer/sendRaw/blockStep.ts
@@ -4,12 +4,11 @@ import { PeerChunk } from '../../chunk';
 import { waitUnblockAll } from './waitUnblockAll';
 
 export async function blockStep<Data = any>(peer: Peer, outcomeChunk: PeerChunk<Data>) {
+  // Добавление в список блокирующих запросов
+  if (outcomeChunk.isBlocker) {
+    peer.blockersChunks.push(outcomeChunk);
+  }
+
   // Ожидание всех блокирующих запросов перед текущим запросом
   if (!outcomeChunk.isForce && peer.hasBlockers) await waitUnblockAll(peer);
-
-  // Добавление в список блокирующих запросов
-  if (!outcomeChunk.isForce && outcomeChunk.isBlocker) {
-    peer.blockersChunks.push(outcomeChunk);
-    // await peer.emitter.emitAsync('block', outcomeChunk);
-  }
 }
